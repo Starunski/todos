dc6dfa535e4502355657c418a321d81db3c36242
Dmitri Starunski <dmitriy.starunskiy@gmail.com>
2020-06-01 18:38:15 +0200

    Updates

---
+++ precache-manifest.2c1cd64593268bf799d2c6828e7a7b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29abde6115d08816c6f191942a12a5d",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "70bca23869cd3cd84446",
+    "url": "/todos/static/css/main.41c7c084.chunk.css"
+  },
+  {
+    "revision": "aff711681964b6542cad",
+    "url": "/todos/static/js/2.b8e813c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.b8e813c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70bca23869cd3cd84446",
+    "url": "/todos/static/js/main.55c8637d.chunk.js"
+  },
+  {
+    "revision": "95433cce65cf95a7482e",
+    "url": "/todos/static/js/runtime-main.e15eb1ae.js"
+  }
+]);
